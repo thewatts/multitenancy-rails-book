30834445763d695f5bfca16a0a67513f5f47a6b9
Nathaniel Watts <reg@nathanielwatts.com>
2013-12-10 22:15:25 -0700

    subdomain validations

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -17,4 +17,20 @@ feature "Accounts" do
     subdomain = "http://test.example.com/subscribem/"
     expect(page.current_url).to eq(subdomain)
   end
+
+  scenario "Ensure subdomain uniqueness" do
+    Subscribem::Account.create!(:subdomain => "test", :name => "Test")
+    visit subscribem.root_path
+    click_link "Account Sign Up"
+    fill_in "Name", with: "Test"
+    fill_in "Subdomain", with: "test"
+    fill_in "Email", with: "subscribem@example.com"
+    fill_in "Password", with: "password", exact: true
+    fill_in "Password confirmation", with: "password"
+    click_button "Create Account"
+    url = "http://www.example.com/subscribem/accounts"
+    expect(page.current_url).to eq(url)
+    expect(page).to have_content "Sorry, your account could not be created."
+    expect(page).to have_content "Subdomain has already been taken"
+  end
 end
